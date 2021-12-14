5056b3c63e184ea36a298e4e98719e621fec54f9
Eelco Koelewijn <eelco@eelcokoelewijn.nl>
2021-12-14 19:09:25 +0100

    chore(eslint): Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-empty-pattern': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': 'off', // since it's fixed after a document save
+  },
+  overrides: [],
+};
